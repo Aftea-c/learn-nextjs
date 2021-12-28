3f4b50cd2ce933025708d855e81990d7087374a8
Dylan <1377927497@qq.com>
2021-12-28 17:28:02 +0800

    init next.js blob

--- README.md
+++ README.md
@@ -1,1 +1,89 @@
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+
+# 这是一份关于 Next.js 的总结
+# Navigate
+
+- 约定式路由
+- Link 标签会进行 prefetch 
+- css in link should add a tag `a` 
+
+
+# Assets,Metadata,and Css
+
+- public 目录下添加 images 可以添加静态资源(图片)
+  
+  use image component from `/next/image` , Images are lazy loaded by default.
+
+  Resizing & optimizing images
+
+  1.也就是说默认开启优化
+  2.不论静态资源有多少图片都不会影响你的编译速度
+  3.只有出现在可视化窗口中时,才会加载(滚动加载优化)
+
+- Metadata
+  
+  1. `<title>` is part of the `<head>`
+  2. Third-Party JavaScript 第三方插件
+     `strategy` 属性设为 `lazyOnload` 可以懒加载
+      `onLoad` 加载完后执行的回调函数
+
+    ```
+    import Script from 'next/script'
+
+     <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+
+    ```
+
+- css (css in js)  Next.js built-in support
+  1. styled-jsx 
+  2. CSS Modules
+  3. Sass  npm install sass  
+     use css modules `.module.scss` or `.module.sass`
+
+- global.css only inside pages/_app.js
+
+- postcss.config.js or tailwind.config.js
+
+# Pre-rendering & Data Fetching
+
+- disable JavaScript on chrome  command + shift + p => javascript => disable javascript 
+  `tell me what do you see`
+- if disable Javascript on create-react-app ,you will see `You need to enable JavaScript to run this app.`
+
+**Two forms of pre-rendering**
+- Server-side rendering            generation HTML on each request 
+- Static Generation rendering      generation HTML on build
+
+
+  Static Generation with Data you should use `getStaticProps` on the page, for example:
+
+  ```
+        export default function Home(props) { ... }
+
+        export async function getStaticProps() {
+        // Get external data from the file system, API, DB, etc.
+        const data = ...
+
+        // The value of the `props` key will be
+        //  passed to the `Home` component
+            return {
+                props: ...
+            }
+        }
+
+  ```
+
+
+  # Dynamic Routes
+
+  `getStaticPaths` get route id
+    
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Link from "next/link";
+import Date from "../components/date";
+import utilStyles from "../styles/utils.module.css";
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <div className="container">
       <Head>
@@ -10,13 +13,26 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read{" "}
+          <Link href="/posts/first-post">
+            <a>this page!</a>
+          </Link>
         </h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
+        <ul>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+        
         <div className="grid">
           <a href="https://nextjs.org/docs" className="card">
             <h3>Documentation &rarr;</h3>
@@ -28,10 +44,7 @@ export default function Home() {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
+          <a href="https://github.com/vercel/next.js/tree/master/examples" className="card">
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
@@ -41,9 +54,7 @@ export default function Home() {
             className="card"
           >
             <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
       </main>
@@ -54,8 +65,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
         </a>
       </footer>
 
@@ -134,8 +144,8 @@ export default function Home() {
           border-radius: 5px;
           padding: 0.75rem;
           font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
+            Bitstream Vera Sans Mono, Courier New, monospace;
         }
 
         .grid {
@@ -195,9 +205,8 @@ export default function Home() {
         body {
           padding: 0;
           margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+            Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
         }
 
         * {
@@ -205,5 +214,16 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
+}
+
+import { getSortedPostsData } from "../lib/posts";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
